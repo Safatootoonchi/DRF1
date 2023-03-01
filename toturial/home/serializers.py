6775ec7b2737b57221565646031ddd97b4a3b1f4
safa <tootoonchisafa@gmail.com>
2023-03-01 20:36:58 +0330

    this is initial code

---
+++ toturial/home/serializers.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from .models import *
+
+
+class CommentSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Comment(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
+        instance.content = validated_data.get('content', instance.content)
+        instance.created = validated_data.get('created', instance.created)
+        instance.save()  # owner=request.
+        return instance
