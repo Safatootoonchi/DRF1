c157d1e58bf12f39bc8653e484c262f9aad8f780
safa <tootoonchisafa@gmail.com>
2023-03-06 00:58:35 +0330

    test3

--- toturial/home/models.py
+++ toturial/home/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 
-class Comment(models.Model):
-    email = models.EmailField()
-    content = models.CharField(max_length=200)
-    created = models.DateTimeField()
+class Album(models.Model):
+    album_name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+
+class Track(models.Model):
+    album = models.ForeignKey(Album, related_name='tracks', on_delete=models.CASCADE)
+    order = models.IntegerField()
+    title = models.CharField(max_length=100)
+    duration = models.IntegerField()
+
+    class Meta:
+        unique_together = ['album', 'order']
+        ordering = ['order']
+
+    def __str__(self):
+        return '%d: %s' % (self.order, self.title)
--- toturial/home/serializers.py
+++ toturial/home/serializers.py
@@ -1,32 +1,10 @@
-from datetime import datetime
 from rest_framework import serializers
-from rest_framework.renderers import JSONRenderer
 from .models import *
 
 
-# class CommentSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#     content = serializers.CharField(max_length=200)
-#     created = serializers.DateTimeField()
-#
-#     def create(self, validated_data):
-#         return Comment.objects.create(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         instance.email = validated_data.get("email", instance.email)
-#         instance.content = validated_data.get('content', instance.content)
-#         instance.created = validated_data.get('created', instance.created)
-#         instance.save()  # owner=request.user
-#         return instance
+class AlbumSerializer(serializers.ModelSerializer):
+    tracks = serializers.StringRelatedField(many=True)
 
-
-class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=100)
-    email = serializers.EmailField()
-
-
-class CommentSerializer(serializers.Serializer):
-    user = UserSerializer(required=False)
-    # edits = EditItemSerializer(many=True)
-    content = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
+    class Meta:
+        model = Album
+        fields = ['album_name', 'artist', 'tracks']
