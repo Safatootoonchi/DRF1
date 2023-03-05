13e89d128237ee2dca38206a0fde8648dec89f89
safa <tootoonchisafa@gmail.com>
2023-03-06 00:33:58 +0330

    test2

--- toturial/home/serializers.py
+++ toturial/home/serializers.py
@@ -4,17 +4,29 @@ from rest_framework.renderers import JSONRenderer
 from .models import *
 
 
-class CommentSerializer(serializers.Serializer):
+# class CommentSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
+#     content = serializers.CharField(max_length=200)
+#     created = serializers.DateTimeField()
+#
+#     def create(self, validated_data):
+#         return Comment.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.email = validated_data.get("email", instance.email)
+#         instance.content = validated_data.get('content', instance.content)
+#         instance.created = validated_data.get('created', instance.created)
+#         instance.save()  # owner=request.user
+#         return instance
+
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100)
     email = serializers.EmailField()
+
+
+class CommentSerializer(serializers.Serializer):
+    user = UserSerializer(required=False)
+    # edits = EditItemSerializer(many=True)
     content = serializers.CharField(max_length=200)
     created = serializers.DateTimeField()
-
-    def create(self, validated_data):
-        return Comment(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.email = validated_data.get("email", instance.email)
-        instance.content = validated_data.get('content', instance.content)
-        instance.created = validated_data.get('created', instance.created)
-        instance.save()  # owner=request.
-        return instance
