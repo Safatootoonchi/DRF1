3a6372c0fa4995dfe6451b0dfc1e34dc2532bf06
safa <tootoonchisafa@gmail.com>
2023-03-07 16:44:56 +0330

    read tutorial serialization

--- toturial/home/serializers.py
+++ toturial/home/serializers.py
@@ -2,9 +2,18 @@ from rest_framework import serializers
 from .models import *
 
 
-class AlbumSerializer(serializers.ModelSerializer):
-    tracks = serializers.StringRelatedField(many=True)
-
+class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Album
-        fields = ['album_name', 'artist', 'tracks']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+    def create(self, validated_data):
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.code = validated_data.get("code", instance.code)
+        instance.linenos = validated_data.get("linenos", instance.linenos)
+        instance.language = validated_data.get("code", instance.language)
+        instance.style = validated_data.get("code", instance.code)
+        instance.save()
+        return instance
--- toturial/home/views.py
+++ toturial/home/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Snippet
+from .serializers import SnippetSerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+@csrf_exempt
+def snippet_list(request):
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        ser_data = SnippetSerializer(instance=snippets, many=True)
+        return JsonResponse(ser_data.data, safe=False)
+    elif request.method == "POST":
+        data = request.data
+        ser_data = SnippetSerializer(data=data)
+        if ser_data.is_valid():
+            ser_data.save()
+            return JsonResponse(ser_data.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(ser_data.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    if request.method == "GET":
+        snippet = get_object_or_404(Snippet, pk=pk)
+        ser_data = SnippetSerializer(instance=snippet)
+        return JsonResponse(ser_data.data, status=status.HTTP_200_OK)
+
+    elif request.method == "PUT":
+        snippet = get_object_or_404(Snippet, pk=pk)
+        data = request.data
+        ser_data = SnippetSerializer(instance=snippet, data=data, partial=True)
+        if ser_data.is_valid():
+            ser_data.save()
+            return JsonResponse(ser_data.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(ser_data.data, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        snippet = get_object_or_404(Snippet, pk=pk)
+        snippet.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
